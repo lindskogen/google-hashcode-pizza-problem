9396865b92b139a437ab327b685b85723c65354b
André Samuelsson <andresamuelsson94@gmail.com>
2018-02-27 18:38:40 +0100

    Add slices, and isValidSlice function

--- src/Pizza.kt
+++ src/Pizza.kt
@@ -1,21 +1,76 @@
-import kotlin.math.max
+import kotlin.coroutines.experimental.buildSequence
+import kotlin.math.abs
 
-class Pizza(val height: Int, val width: Int, val minCellsPerIngredient: Int, val maxCells: Int, val grid: List<List<Ingredient>>) {
+typealias Slice = Pair<Pizza.Coordinate, Pizza.Coordinate>
+typealias Row = List<Pizza.Ingredient>
+typealias Grid = List<Row>
 
-    enum class Ingredient{
-        Tomato{
+class Pizza(
+        private val height: Int,
+        private val width: Int,
+        private val minCellsPerIngredient: Int,
+        private val maxCells: Int,
+        private val grid: Grid,
+        private val slices: List<Slice> = emptyList()) {
+
+    data class Coordinate(val x: Int, val y: Int)
+
+
+
+    fun Slice.area(): Int {
+        return abs(this.first.x - this.second.x) * abs(this.first.y - this.second.y)
+    }
+
+    fun Slice.coordinatesList(): Sequence<Coordinate> {
+        val second = this.second
+        val first = this.first
+        return buildSequence {
+            (first.x .. second.x).forEach { x ->
+                (first.y .. second.y).forEach {y ->
+                    yield(Coordinate(x,y))
+                }
+
+            }
+        }
+
+    }
+
+    enum class Ingredient {
+        Tomato {
             override fun toString(): String {
                 return "T"
             }
         },
-        Mushroom{
+        Mushroom {
             override fun toString(): String {
                 return "M"
             }
         }
 
     }
+    
 
+    fun isValidSlice(slice: Slice): Boolean {
+        if (slice.area() < 2*this.minCellsPerIngredient || slice.area() > this.maxCells) {
+            return false
+        }
+
+        var countTomato = 0
+        var countMushroom = 0
+
+        for (coord in slice.coordinatesList()) {
+            when(this.grid[coord.x][coord.y]) {
+                Ingredient.Tomato -> countTomato++
+                Ingredient.Mushroom -> countMushroom++
+            }
+
+            if (countTomato >= this.minCellsPerIngredient && countMushroom >= this.minCellsPerIngredient) {
+                return true
+            }
+        }
+
+        return !(countTomato < this.minCellsPerIngredient || countMushroom < this.minCellsPerIngredient)
+    }
 
     override fun toString(): String {
         val res = StringBuilder()
@@ -31,7 +86,7 @@ class Pizza(val height: Int, val width: Int, val minCellsPerIngredient: Int, val
         res.append("\n\t")
         grid.forEachIndexed { i, row ->
             row.forEach { cell -> res.append(cell) }
-            res.append(if (i != grid.size-1) "\n\t" else "\n")
+            res.append(if (i != grid.size - 1) "\n\t" else "\n")
         }
         res.append("}")
 
@@ -46,7 +101,7 @@ class Pizza(val height: Int, val width: Int, val minCellsPerIngredient: Int, val
             val header = lines[0].split(" ")
             val unparsedPizza = lines.drop(1)
 
-            val parsedPizza = unparsedPizza.map{ line ->
+            val parsedPizza = unparsedPizza.map { line ->
                 line.map { cell ->
                     if (cell == 'T') {
                         Ingredient.Tomato
--- src/main.kt
+++ src/main.kt
@@ -5,4 +5,5 @@ fun main(args: Array<String>) {
     val pizza = Pizza.parse(File("example.in").readText())
     println("Hello, world!")
     println(pizza)
+    println(pizza.isValidSlice(Slice(Pizza.Coordinate(0,0), Pizza.Coordinate(1,1))))
 }
